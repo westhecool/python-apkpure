6f0ffb39dfece023432ee79667684cb40afac7ec
westhecool <52859033+westhecool@users.noreply.github.com>
2024-06-01 20:29:44 -0400

    start working on code

---
+++ main.py
@@ -0,0 +1,44 @@
+import bs4
+import requests
+
+def get_info(id):
+    url = f'https://apkpure.net/null/{id}/versions'
+    r = requests.get(url)
+    soup = bs4.BeautifulSoup(r.text, 'html.parser')
+    title = soup.find('div', class_='app-name').find('h1').text
+    type = soup.find('div', class_='app-name').find('span').text.lower()
+    developer = soup.find('a', class_='developer').text
+    date = soup.find('div', class_='date').text.lower()
+    versions = []
+    for i in soup.find_all('li', class_='version'):
+        url = 'https://apkpure.net/' + i.find('a', class_='download-btn')['href']
+        version = i.find('div', class_='code-info').find('span', class_='name').text.lower()
+        version_code = i.find('div', class_='code-info').find('span', class_='code').text.lower().replace('(', '').replace(')', '')
+        type = i.find('div', class_='code-info').find('span', class_='tag').text.lower()
+        t = i.find('div', class_='additional-info').find_all('span')
+        date = None
+        size = None
+        android_version = None
+        if len(t) >= 1:
+            date = t[0].text.lower()
+        if len(t) >= 2:
+            size = t[1].text.lower()
+        if len(t) >= 3:
+            android_version = t[2].text.lower()
+        versions.append({
+            'url': url,
+            'version': version,
+            'version_code': version_code,
+            'type': type,
+            'date': date,
+            'size': size,
+            'android_version': android_version
+        })
+    return {
+        'versions': versions,
+        'title': title,
+        'type': type,
+        'developer': developer,
+        'date': date
+    }
+print(get_info('com.garena.game.bc'))
