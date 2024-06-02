fcb6a29a7b9083779b30ac7b2e245e13cc690d67
westhecool <52859033+westhecool@users.noreply.github.com>
2024-06-01 21:24:42 -0400

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,5 +1,10 @@
 import argparse
 import main
+import re
+def make_safe_filename(filename):
+    invalid_chars = r'[<>:"/\\|?*]'
+    safe_filename = re.sub(invalid_chars, '', filename)
+    return safe_filename
 
 args = argparse.ArgumentParser()
 args.add_argument("command", choices=["download"])
@@ -8,5 +13,6 @@ args = args.parse_args()
 
 if args.command == "download":
     info = main.get_info(args.id)
-    print(f"Downloading {info['title']} v{info['versions'][0]['version']}...")
-    main.download(info["versions"][0]["url"], f"./{args.id} v{info['versions'][0]['version']}.{info['versions'][0]['type']}")
+    print(f"Downloading {info['title']} v{info['versions'][0]['version']}...", end='')
+    main.download(info["versions"][0]["url"], f"./{make_safe_filename(info['title'])} v{info['versions'][0]['version']}.{info['versions'][0]['type']}")
+    print("done!")
