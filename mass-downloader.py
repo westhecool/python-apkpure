9f3a32031b5d12217c942a05191105ca53d6768a
westhecool <52859033+westhecool@users.noreply.github.com>
2024-06-02 09:20:30 -0400

    add failed list

--- mass-downloader.py
+++ mass-downloader.py
@@ -1,15 +1,18 @@
 import os
 import shutil
 import sys
-f = open("download-list.txt", "r")
-for line in f.readlines():
+list = open("download-list.txt", "r")
+failed = open("failed.txt", "w")
+for line in list.readlines():
     id = line.strip()
     if os.path.exists(f"./apks/{id}"):
         continue
     if os.system(f"{sys.executable} cli.py download {id} -d ./apks/{id}") != 0:
         print("Failed to download", id)
+        failed.write(id + "\n")
         try:
             shutil.rmtree(f"./apks/{id}")
         except:
             pass
-f.close()
+list.close()
+failed.close()
