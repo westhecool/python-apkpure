b3b892f2e989f11abde51e2a26600aabe7c7ce63
westhecool <52859033+westhecool@users.noreply.github.com>
2024-08-02 19:20:29 -0400

    fix extra space in file name when specifying custom output file name

--- cli.py
+++ cli.py
@@ -19,5 +19,5 @@ os.makedirs(args.output_dir, exist_ok=True)
 if args.command == "download":
     info = main.get_info(args.id)
     print(f"Downloading {info['title']} v{info['versions'][0]['version']}...", end='', flush=True)
-    main.download(info["versions"][0]["url"], f"{args.output_dir}/{make_safe_filename(info['title']) if not args.output_file else args.output_file} {'v' + info['versions'][0]['version'] if not args.output_file else ''}", not args.output_file)
+    main.download(info["versions"][0]["url"], f"{args.output_dir}/{make_safe_filename(info['title']) if not args.output_file else args.output_file}{' v' + info['versions'][0]['version'] if not args.output_file else ''}", not args.output_file)
     print("done!", flush=True)
